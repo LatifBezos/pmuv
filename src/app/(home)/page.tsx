8e1293269c66b325309ab4cadd0cd80e2d1503d8
JoCode-dev <46027270+JoCode-dev@users.noreply.github.com>
2025-03-26 17:42:48 +0000

    feat: Add Navbar

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,9 @@
+import { HomeNavbar } from "@/layout/home/ui/components/home-navbar";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center">
+      <HomeNavbar />
+    </main>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import {
+  Geist,
+  Geist_Mono,
+  Ubuntu,
+  Merriweather,
+  Playfair_Display,
+} from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -10,6 +16,24 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const ubuntu = Ubuntu({
+  variable: "--font-ubuntu",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700"],
+});
+
+const merriweather = Merriweather({
+  variable: "--font-merriweather",
+  subsets: ["latin"],
+  weight: ["300", "400", "700"],
+});
+
+const playfairDisplay = Playfair_Display({
+  variable: "--font-playfair-display",
+  subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 export const metadata: Metadata = {
@@ -25,7 +49,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${ubuntu.variable} ${merriweather.variable} ${playfairDisplay.variable} antialiased`}
       >
         {children}
       </body>
