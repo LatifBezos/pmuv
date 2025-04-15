b805d264a84fa050c114471db57a4310b1aeb9f8
LatifBezos <latiftondossama@gmail.com>
2025-04-15 10:15:34 +0000

    upd latif

--- src/layout/dashboard/ui/sections/dashboard-section/profile-section.tsx
+++ src/layout/dashboard/ui/sections/dashboard-section/profile-section.tsx
@@ -20,8 +20,6 @@ export function ProfileSection() {
           <div className="flex flex-col">
             <h1 className="text-2xl font-semibold">
               Hi, Latif Tondossama
-            <h1 className="text-xl sm:text-2xl font-semibold truncate">
-              Hi, Ange Cédric Joël DUAKON
             </h1>
             <a
               href="https://payemoiunverre.com/superlatif"
